8adb586ac78287fff3cfb2eb625026ae0231bd86
JorshuaG <joshbgearheart@gmail.com>
2021-08-23 10:30:02 -1000

    fixed repeat error

--- watermyplants/src/actions/index.js
+++ watermyplants/src/actions/index.js
@@ -36,55 +36,46 @@ export const signUp = (userInfo) => (dispatch) => {
     });
 };
 
-
-
-    dispatch({ type: START_REQUEST })
-    axios.post("", userInfo) //insert url here
-        .then(res => {
-            dispatch({ type: FINISH_AUTHENTICATION, payload: res.data }) // may need to adjust this later
-        })
-        .catch(err => {
-            dispatch({ type: FAILED_REQUEST, payload: err })
-        })
-}
-
 export const UPDATE_PLANT = "UPDATE_PLANT";
 
 export const updatePlant = (plantData) => (dispatch) => {
-    dispatch({ type: START_REQUEST })
-    axiosWithAuth().put("", plantData)
-        .then(res => {
-            dispatch({ type: UPDATE_PLANT, payload: res.data })
-        })
-        .catch(err => {
-            dispatch({ type: FAILED_REQUEST, payload: err })
-        })
-}
+  dispatch({ type: START_REQUEST });
+  axiosWithAuth()
+    .put("", plantData)
+    .then((res) => {
+      dispatch({ type: UPDATE_PLANT, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FAILED_REQUEST, payload: err });
+    });
+};
 export const UPDATE_USER = "UPDATE_USER";
 
 export const updateUser = (userData) => (dispatch) => {
-    dispatch({ type: START_REQUEST })
-    axiosWithAuth().put("", userData)
-        .then(res => {
-            dispatch({ type: UPDATE_USER, payload: res.data })
-        })
-        .catch(err => {
-            dispatch({ type: FAILED_REQUEST, payload: err })
-        })
-}
+  dispatch({ type: START_REQUEST });
+  axiosWithAuth()
+    .put("", userData)
+    .then((res) => {
+      dispatch({ type: UPDATE_USER, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FAILED_REQUEST, payload: err });
+    });
+};
 
 export const DELETE_PLANT = "DELETE_PLANT";
 
 export const deletePlant = (plantData) => (dispatch) => {
-    dispatch({ type: START_REQUEST })
-    axiosWithAuth().delete("", plantData)
-        .then(res => {
-            dispatch({ type: DELETE_PLANT, payload: res.data })
-        })
-        .catch(err => {
-            dispatch({ type: FAILED_REQUEST, payload: err })
-        })
-}
+  dispatch({ type: START_REQUEST });
+  axiosWithAuth()
+    .delete("", plantData)
+    .then((res) => {
+      dispatch({ type: DELETE_PLANT, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FAILED_REQUEST, payload: err });
+    });
+};
 
 export const logOut = () => (dispatch) => {
   axios.post("").then((res) => {

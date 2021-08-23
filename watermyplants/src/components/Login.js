ffb6caa70d08ee54c8fd541dd7b4f3438e9da3cc
JorshuaG <joshbgearheart@gmail.com>
2021-08-23 08:18:38 -1000

    basic functionality of login still need endpoints

--- watermyplants/src/components/Login.js
+++ watermyplants/src/components/Login.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useForm } from "../hooks/useForm";
+
+const initialLogin = {
+  username: "",
+  password: "",
+};
 
 function Login(props) {
-  return <div></div>;
+  const [loginData, handleChange] = useForm(initialLogin);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //   axios post
+    // set token to localstorage
+    //push to plantlist
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          Username:<input type="text"></input>
+        </label>
+        <label>
+          Password:<input type="password"></input>
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;

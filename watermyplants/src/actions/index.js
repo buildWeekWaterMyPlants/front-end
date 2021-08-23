d361ba51a26a5f58157c80d381b2e547ea6f828f
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-08-23 14:16:21 -0400

    add async actions

--- watermyplants/src/actions/index.js
+++ watermyplants/src/actions/index.js
@@ -1,5 +1,22 @@
+import axios from "axios";
+
 export const CHECK_AUTHORIZATION = "CHECK_AUTHORIZATION";
 export const checkAuth = (authenticated) => ({
     type: CHECK_AUTHORIZATION, 
     payload: authenticated
 })
+
+export const START_LOGIN = "START_LOGIN";
+export const FINISH_LOGIN = "FINISH_LOGIN";
+export const FAILED_LOGIN = "FAILED_LOGIN";
+
+export const login = () => (dispatch) => {
+    dispatch({ type: START_LOGIN })
+    axios.post("") //insert url here
+        .then(res => {
+            dispatch({ type: FINISH_LOGIN, payload: res.data }) // may need to adjust this later
+        })
+        .catch(err => {
+            dispatch({ type: FAILED_LOGIN, payload: err })
+        })
+}

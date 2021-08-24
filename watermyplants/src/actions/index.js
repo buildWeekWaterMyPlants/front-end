6c5e7ae586ac9783397b39f3aab214272659dee2
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-08-24 13:00:45 -0400

    added plant actions

--- watermyplants/src/actions/index.js
+++ watermyplants/src/actions/index.js
@@ -8,8 +8,9 @@ export const checkAuth = (authenticated) => ({
 });
 
 export const START_REQUEST = "START_REQUEST";
-export const FINISH_AUTHENTICATION = "FINISH_AUTHENTICATION";
+export const AUTHENTICATE = "AUTHENTICATE";
 export const FAILED_REQUEST = "FAILED_REQUEST";
+export const FINISH_REQUEST = "FINISH_REQUEST";
 export const LOGOUT = "LOGOUT";
 
 export const login = (userInfo) => (dispatch) => {
@@ -17,11 +18,14 @@ export const login = (userInfo) => (dispatch) => {
   axios
     .post("", userInfo) //insert url here
     .then((res) => {
-      dispatch({ type: FINISH_AUTHENTICATION, payload: res.data }); // may need to adjust this later
+      dispatch({ type: AUTHENTICATE, payload: res.data }); // may need to adjust this later
     })
     .catch((err) => {
       dispatch({ type: FAILED_REQUEST, payload: err });
-    });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
 };
 
 export const signUp = (userInfo) => (dispatch) => {
@@ -29,11 +33,14 @@ export const signUp = (userInfo) => (dispatch) => {
   axios
     .post("", userInfo) //insert url here
     .then((res) => {
-      dispatch({ type: FINISH_AUTHENTICATION, payload: res.data }); // may need to adjust this later
+      dispatch({ type: AUTHENTICATE, payload: res.data }); // may need to adjust this later
     })
     .catch((err) => {
       dispatch({ type: FAILED_REQUEST, payload: err });
-    });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
 };
 
 export const UPDATE_PLANT = "UPDATE_PLANT";
@@ -47,20 +54,10 @@ export const updatePlant = (plantData) => (dispatch) => {
     })
     .catch((err) => {
       dispatch({ type: FAILED_REQUEST, payload: err });
-    });
-};
-export const UPDATE_USER = "UPDATE_USER";
-
-export const updateUser = (userData) => (dispatch) => {
-  dispatch({ type: START_REQUEST });
-  axiosWithAuth()
-    .put("", userData)
-    .then((res) => {
-      dispatch({ type: UPDATE_USER, payload: res.data });
     })
-    .catch((err) => {
-      dispatch({ type: FAILED_REQUEST, payload: err });
-    });
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
 };
 
 export const DELETE_PLANT = "DELETE_PLANT";
@@ -74,11 +71,53 @@ export const deletePlant = (plantData) => (dispatch) => {
     })
     .catch((err) => {
       dispatch({ type: FAILED_REQUEST, payload: err });
-    });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
+};
+
+
+export const ADD_PLANT = "ADD_PLANT";
+
+export const addPlant = (plantData) => (dispatch) => {
+  dispatch({ type: START_REQUEST });
+  axiosWithAuth()
+    .post("", plantData)
+    .then((res) => {
+      dispatch({ type: ADD_PLANT, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FAILED_REQUEST, payload: err });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
+};
+
+export const UPDATE_USER = "UPDATE_USER";
+
+export const updateUser = (userData) => (dispatch) => {
+  dispatch({ type: START_REQUEST });
+  axiosWithAuth()
+    .put("", userData)
+    .then((res) => {
+      dispatch({ type: UPDATE_USER, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FAILED_REQUEST, payload: err });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
 };
 
 export const logOut = () => (dispatch) => {
-  axios.post("").then((res) => {
-    dispatch({ type: LOGOUT });
-  });
+  axios.post("")
+    .then(() => {
+      dispatch({ type: LOGOUT });
+    })
+    .finally(() => {
+      dispatch({ type: FINISH_REQUEST })
+    })
 };
--- watermyplants/src/reducers/index.js
+++ watermyplants/src/reducers/index.js
@@ -1,13 +1,18 @@
 import {
   CHECK_AUTHORIZATION,
-  FINISH_AUTHENTICATION,
+  AUTHENTICATE,
+  FINISH_REQUEST,
   START_REQUEST,
+  ADD_PLANT,
+  DELETE_PLANT,
+  UPDATE_PLANT,
   LOGOUT,
 } from "../actions";
 
 const initialState = {
   authenticated: localStorage.getItem("token") || false,
   makingRequest: false,
+  plants: []
 };
 
 const reducer = (state = initialState, actions) => {
@@ -22,13 +27,36 @@ const reducer = (state = initialState, actions) => {
         ...state,
         makingRequest: true,
       };
-    case FINISH_AUTHENTICATION:
+    case FINISH_REQUEST:
+      return {
+        ...state,
+        makingRequest: false,
+      };
+    case AUTHENTICATE:
       localStorage.setItem("token", actions.payload);
       return {
         ...state,
-        authenticated: true,
-        makingRequest: false,
+        authenticated: true
       };
+    case ADD_PLANT:
+      return {
+        ...state,
+        plants: [...state.plants, actions.payload]
+      }
+    case DELETE_PLANT:
+      return {
+        ...state,
+        plants: state.plants.filter(plant => 
+          plant.id !== actions.payload.id
+        )
+      }
+    case UPDATE_PLANT:
+      return {
+        ...state,
+        plants: state.plants.map(plant => 
+          plant.id !== actions.payload.id ? plant : actions.payload
+        )
+      }
     case LOGOUT:
       localStorage.removeItem("token");
       return { ...state, authenticated: false, makingRequest: false };
